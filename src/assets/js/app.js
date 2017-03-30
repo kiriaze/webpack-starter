5549f8ade5ed1eabd1c8af7d9d3db5edd4989dd7
Constantine <ckiriaze@basicagency.com>
2017-03-30 16:59:12 -0700

    clean up

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,7 +7,6 @@ import '../scss/style.scss';
 // import '../../views/modules/style.scss';
 
 import jQuery from 'jquery';
-import people from './people';
 
 
 // import mods script.js, which imports the associated style.scss
@@ -16,18 +15,3 @@ import {element} from '../../views/modules/heroes/hero/script';
 document.write(element);
 
 // import '../../views/modules/heroes/hero/template.twig'; // requires twig-loader
-
-var args = {
-	foo: 'bar'
-};
-
-$('#app').css({
-	'opacity': .3
-});
-
-const app			= document.querySelector('#app');
-if ( app ) {
-	app.innerHTML	= `<pre>${JSON.stringify(people, null, 2)}</pre>`;
-}
-
-console.log(args, 'test');
