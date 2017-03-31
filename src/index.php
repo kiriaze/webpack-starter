ac2ca9b2b3b1925b58bb69ffae5a4e10e279755e
Constantine <ckiriaze@gmail.com>
2017-03-31 00:11:12 -0700

    twig and slim working, data working, dynamic routing working. dope.

--- src/index.php
+++ src/index.php
@@ -1,40 +1,71 @@
 <?php
 
+// https://akrabat.com/wp-content/uploads/2015-05-20-phpberks-getting-started-with-slim-3.pdf
+
 require '../vendor/autoload.php';
 
-// // Create app
-// $app = new \Slim\App();
+$config = [
+	'settings' => [
+		'displayErrorDetails' => true
+	]
+];
 
-// // Get container
-// $container = $app->getContainer();
+$app = new \Slim\App($config);
 
-// // Register component on container
-// $container['view'] = function ($container) {
-//     $view = new \Slim\Views\Twig('views/templates', [
-//         'cache' => false
-//     ]);
-    
-//     // Instantiate and add Slim specific extension
-//     $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
-//     $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
+$container = $app->getContainer();
 
-//     return $view;
-// };
+$container['view'] = function ($container) {
+	
+	$templates  = __DIR__ . '/views/';
 
-// // Render Twig template in route
-// $app->get('/', function ($request, $response, $args) {
-//     return $this->view->render($response, 'views/pages/index.html');
-// })->setName('index');
+	$args 		= [
+		'debug' => true, // This line should enable debug mode
+		'cache' => false
+	];
 
-// // Run app
-// $app->run();
+	$view = new Slim\Views\Twig($templates, $args);
 
+	return $view;
 
+};
 
-$app = new \Slim\App;
+// get json data
+function getData() {
 
+	$data = [];
+	$files = scandir(__DIR__ . '/assets/data');
+
+	foreach( $files as $file ) {
+
+		if ( $file != '.' && $file != '..' ) {
+			
+			$json = file_get_contents( __DIR__ . '/assets/data/' . $file );
+			
+			$name = substr($file, 0, -5);
+
+			$data['data'][$name] = json_decode($json, true);
+
+		}
+	}
+
+	return $data;
+}
+
+// index
 $app->get('/', function($request, $response) {
-	return 'hello world';
+
+	$data = getData();
+
+	return $this->view->render($response, 'pages/index.html', $data);
+
+});
+
+// dynamic routing
+$app->get('/{name}', function($request, $response, $args) {
+	$name = $args['name'];
+	$name = htmlspecialchars($name);
+	$data = getData();
+	return $this->view->render($response, 'pages/'. $name .'.html', $data);
 });
 
 $app->run();
