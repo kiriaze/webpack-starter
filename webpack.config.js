2585176733d2acbfe0bbc3d7b564904b8c0ad0e1
Constantine <ckiriaze@gmail.com>
2018-10-23 21:08:19 -0700

    fixed webpack dev env output port issue

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ const config = {
 	output: {
 		path: path.resolve(__dirname, baseConfig.src),
 		filename: 'assets/js/[name].bundle.js',
-		publicPath: 'http://localhost:3000/',
+		publicPath: `http://${config.proxy ? ip : config.localhost}:${config.port.webpack}/`
 		chunkFilename: 'assets/js/common.js'
 	},
 	
