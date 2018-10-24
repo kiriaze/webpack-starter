7915355ebf86750ee2625a12f7c7d9ce4b9e8411
Constantine <ckiriaze@gmail.com>
2018-10-23 21:09:41 -0700

    minor update

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ const config = {
 	output: {
 		path: path.resolve(__dirname, baseConfig.src),
 		filename: 'assets/js/[name].bundle.js',
-		publicPath: `http://${config.proxy ? ip : config.localhost}:${config.port.webpack}/`,
+		publicPath: `http://${baseConfig.proxy ? ip : baseConfig.localhost}:${baseConfig.port.webpack}/`,
 		chunkFilename: 'assets/js/common.js'
 	},
 	
